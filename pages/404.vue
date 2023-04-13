3137739f94b8f4fb15695f0b3ab3eef8ea39f34b
Hamza-Harzi <97437135+Hamza-Harzi@users.noreply.github.com>
2023-04-13 06:34:01 +0200

    choose the best resto with nuxt.js3

---
+++ pages/404.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div class="container">
+      <h1>Page not Found</h1>
+      <NuxtLink href="/">Go Back</NuxtLink>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  text-align: center;
+  margin-top: 5rem;
+}
+</style>
---
+++ pages/restaurants/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1>Hello this is the restaurants</h1>
+    <div class="container">
+      <RestaurantTable />
+    </div>
+  </div>
+</template>
