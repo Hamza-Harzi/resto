240fa79ad4071f8ea4d66e81cf52acb7b54854c2
Hamza-Harzi <97437135+Hamza-Harzi@users.noreply.github.com>
2023-04-13 07:24:04 +0200

    using the custom layout

--- pages/restaurants/index.vue
+++ pages/restaurants/index.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
-    <h1>Hello this is the restaurants</h1>
-    <div class="container">
-      <RestaurantTable />
-    </div>
+    <NuxtLayout name="custom">
+      <h1>Hello this is the restaurants</h1>
+      <div class="container">
+        <RestaurantTable />
+      </div>
+    </NuxtLayout>
   </div>
 </template>
