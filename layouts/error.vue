9ddaf27485c150f8ebc82838e9ad025c10d9f388
Hamza-Harzi <97437135+Hamza-Harzi@users.noreply.github.com>
2023-04-13 08:01:14 +0200

    Injecting Custom Elements to Layout Page

---
+++ layouts/error.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <slot name="header" />
+    <div>
+      <img
+        src="https://previews.123rf.com/images/alvincadiz/alvincadiz1604/alvincadiz160400358/56426319-vector-illustration-of-smiley-emoticon-sad-face.jpg"
+        alt=""
+      />
+    </div>
+    <slot name="redirectEl" />
+  </div>
+</template>
+
+<style scoped>
+img {
+  width: 10rem;
+}
+</style>
